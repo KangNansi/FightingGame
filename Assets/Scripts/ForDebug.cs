6ad2eda3e433b52462075b2b4cc2eaaa7e611c2e
Kangnansi <evillz@hotmail.com>
2017-11-28 12:14:27 +0100

    @kavehes: Debug slowdown on X

--- Assets/Scripts/ForDebug.cs
+++ Assets/Scripts/ForDebug.cs
@@ -16,7 +16,7 @@ public class ForDebug : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.S)) {
+        if (Input.GetKeyDown(KeyCode.X)) {
             SlowMo = !SlowMo;
             ChangeTime();
         }
