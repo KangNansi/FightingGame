df0a283b02175d765fb3c0ba06d8c56c8c0ba91b
KangNansi <evillz@hotmail.com>
2017-12-15 05:41:29 +0100

    Poum

---
+++ Assets/Editor/Scripts/Tools/NoiseBoxWindow.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+public class NoiseBoxWindow : EditorWindow {
+
+    NoiseBox noise;
+    private static Vector2 scrollPosition;
+
+    [OnOpenAssetAttribute(1)]
+    public static bool step1(int instanceID, int line)
+    {
+        UnityEngine.Object obj = EditorUtility.InstanceIDToObject(instanceID);
+        if (obj is NoiseBox)
+        {
+            NoiseBoxWindow window = EditorWindow.GetWindow<NoiseBoxWindow>();
+            window.noise = (NoiseBox)obj;
+            return true;
+        }
+        return false; // we did not handle the open
+    }
+
+    [MenuItem("Assets/Create/Noise")]
+    public static void CreateMyAsset()
+    {
+        NoiseBox asset = ScriptableObject.CreateInstance<NoiseBox>();
+
+        // AssetDatabase.CreateAsset(asset, "Assets/Fighter.asset");
+        //AssetDatabase.SaveAssets();
+        ProjectWindowUtil.CreateAsset(asset, "Assets/Noise.asset");
+
+        //EditorUtility.FocusProjectWindow();
+
+        //Selection.activeObject = asset;
+    }
+
+    [MenuItem("Window/Noise Creator %#n")]
+    static void Init()
+    {
+        NoiseBoxWindow window = EditorWindow.GetWindow<NoiseBoxWindow>();
+    }
+
+    private void OnGUI()
+    {
+        noise = (NoiseBox)EditorGUILayout.ObjectField(noise, typeof(NoiseBox), true);
+        NoiseBoxEditor(noise);
+    }
+
+    public static bool NoiseBoxEditor(NoiseBox nb)
+    {
+        if(nb == null)
+        {
+            return false;
+        }
+        EditorGUI.BeginChangeCheck();
+
+        scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
+        if (GUILayout.Button("Add"))
+        {
+            nb.noises.Add(new NoiseMaterial());
+        }
+        foreach (NoiseMaterial n in nb.noises)
+        {
+            NoiseEditor(n);
+        }
+        EditorUtility.SetDirty(nb);
+        EditorGUILayout.EndScrollView();
+        return EditorGUI.EndChangeCheck();
+    }
+
+    static void NoiseEditor(NoiseMaterial n, bool colored = true)
+    {
+        EditorGUILayout.Separator();
+        n.color = EditorGUILayout.ColorField(n.color);
+        n.noise.m_frequency = EditorGUILayout.Slider("Frequency:", n.noise.m_frequency, 0, 100);
+        n.strength = EditorGUILayout.Slider("Strength:", n.strength, 0, 100);
+        n.pureStrength = EditorGUILayout.Slider("Pure Strength:", n.pureStrength, 0, 100);
+        n.exageration = EditorGUILayout.Slider("Exageration:", n.exageration, 0, 100);
+        n.invert = EditorGUILayout.Toggle("Invert:", n.invert);
+        n.rotation = EditorGUILayout.Slider("Rotation:", n.rotation, 0, 360);
+        n.scale = EditorGUILayout.Vector2Field("Scale:", n.scale);
+        n.noise.m_noiseType = (FastNoise.NoiseType)EditorGUILayout.EnumPopup("Type:", n.noise.m_noiseType);
+        n.noise.m_seed = EditorGUILayout.IntField(n.noise.m_seed);
+        if(n.noise.m_noiseType == FastNoise.NoiseType.Linear)
+        {
+            n.noise.m_linearType = (FastNoise.LinearType)EditorGUILayout.EnumPopup("Linear Type:", n.noise.m_linearType);
+        }
+        n.noise.m_fractalType = (FastNoise.FractalType)EditorGUILayout.EnumPopup("FType:", n.noise.m_fractalType);
+        EditorGUILayout.Separator();
+    }
+}
---
+++ Assets/Scripts/Tools/NoiseBox.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class NoiseMaterial
+{
+    public FastNoise noise = new FastNoise();
+    public float strength = 1.0f;
+    public float pureStrength = 1.0f;
+    public bool invert = false;
+    public float rotation = 0;
+    public Vector2 scale = new Vector2(1, 1);
+    public float exageration = 1.0f;
+
+    public Color color = Color.white;
+
+    public float GetValue(float x, float y)
+    {
+        Vector2 finalPos = new Vector2(x, y);
+        finalPos = Quaternion.AngleAxis(rotation, Vector3.forward) * finalPos;
+        finalPos.Scale(scale);
+        float r = noise.GetNoise(finalPos.x, finalPos.y) * strength;
+        if (invert) r = 1 - r;
+
+        r -= 0.5f;
+        r *= exageration;
+        r += 0.5f;
+
+        r = Mathf.Clamp(r, 0, 1);
+        return r * pureStrength;
+    }
+}
+
+public class NoiseBox : ScriptableObject {
+
+    public List<NoiseMaterial> noises = new List<NoiseMaterial>();
+    public bool colored = true;
+
+    public float GetValue(float x, float y)
+    {
+        float result = 0;
+        float total = 0;
+        foreach(NoiseMaterial n in noises)
+        {
+            result += n.GetValue(x, y);
+            total += n.strength;
+        }
+        return result / total;
+    }
+
+    public Color GetColor(float x, float y)
+    {
+        Color color = new Color();
+        float total = 0;
+        foreach(NoiseMaterial n in noises)
+        {
+            color += n.GetValue(x, y) * n.color;
+            total += n.strength;
+        }
+
+        return color/total;
+    }
+}
