200fce5a7e7c8da8b1d2760e45d4bee78630b4e2
KangNansi <evillz@hotmail.com>
2018-02-11 20:43:55 +0100

    ui

--- Assets/UI/Scripts/UIFighterState.cs
+++ Assets/UI/Scripts/UIFighterState.cs
@@ -9,7 +9,13 @@ namespace FightingGame
     {
         public FighterController fighter;
         public Image LifeBar;
+        public Vector3 lifeStartPosition;
+        public RectTransform lifeMask;
+        public Vector3 lifeMaskStart;
         public Image ComboBar;
+        Vector3 comboStartPosition;
+        public RectTransform comboMask;
+        Vector3 comboMaskStart;
         public Image GuardBar;
         public Image StunBar;
         public Image ChargeBar;
@@ -18,16 +24,24 @@ namespace FightingGame
         // Use this for initialization
         void Start()
         {
+            lifeMaskStart = lifeMask.transform.position;
+            lifeStartPosition = LifeBar.transform.position;
+            comboStartPosition = ComboBar.transform.position;
+
             victoryBoxes.SetFighter(fighter);
         }
 
         // Update is called once per frame
         void Update()
         {
-            LifeBar.fillAmount = fighter.Life;
-            LifeBar.color = Color.HSVToRGB((fighter.Life * 128) / 360f, 1f, 1f);
-            ComboBar.fillAmount = fighter.ComboStrength;
-            ComboBar.rectTransform.anchoredPosition = new Vector3(LifeBar.rectTransform.rect.width * (1f - fighter.Life), 0, 0);
+            lifeMask.anchoredPosition = new Vector3((1f-fighter.Life) * LifeBar.rectTransform.rect.width, 0, 0);
+            comboMask.anchoredPosition = new Vector3(((1f - fighter.Life) + fighter.ComboStrength) * LifeBar.rectTransform.rect.width, 0, 0);
+            LifeBar.rectTransform.position = lifeStartPosition;
+            ComboBar.rectTransform.position = comboStartPosition;
+            //LifeBar.fillAmount = fighter.Life;
+            //LifeBar.color = Color.HSVToRGB((fighter.Life * 128) / 360f, 1f, 1f);
+            //ComboBar.fillAmount = fighter.ComboStrength;
+            //ComboBar.rectTransform.anchoredPosition = new Vector3(LifeBar.rectTransform.rect.width * (1f - fighter.Life), 0, 0);
 
             GuardBar.fillAmount = fighter.Guard;
             StunBar.fillAmount = fighter.Stun;
