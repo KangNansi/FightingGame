18e6a2262a6df6c52d7e571fb4d0a5fa8078f6f7
Boby <alexandre.bobe@laposte.net>
2018-02-11 20:38:10 +0100

    New sounds

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -15,11 +15,20 @@ namespace AK
     {
         static const AkUniqueID BACK_KICK_INIT = 4195553302U;
         static const AkUniqueID BACK_KICK_WHOOSH = 3992044314U;
+        static const AkUniqueID BIGCONFIRM = 1795338027U;
         static const AkUniqueID BLOCK = 4001580976U;
+        static const AkUniqueID CONFIRM = 1517537939U;
+        static const AkUniqueID DASH = 1942692385U;
         static const AkUniqueID FORWARD_INIT = 1593604493U;
         static const AkUniqueID FORWARD_WHOOSH = 17683061U;
+        static const AkUniqueID GETUP = 1867622356U;
+        static const AkUniqueID GUARDBREAK = 3067181449U;
+        static const AkUniqueID HEAVY_HIT = 1501246120U;
         static const AkUniqueID JAB = 780552104U;
+        static const AkUniqueID JUMP_INIT = 164547642U;
+        static const AkUniqueID JUMP_LAND = 3419686739U;
         static const AkUniqueID KILL = 2030840469U;
+        static const AkUniqueID LIGHT_HIT = 3594083121U;
         static const AkUniqueID LOW_KICK = 622371194U;
         static const AkUniqueID MID_HIT = 1711038907U;
         static const AkUniqueID PARRY_11 = 4268837494U;
@@ -27,6 +36,7 @@ namespace AK
         static const AkUniqueID ROUND_START = 280570306U;
         static const AkUniqueID UPPER_KICK = 321053796U;
         static const AkUniqueID UPPER_KICK_HANDPAT = 587916051U;
+        static const AkUniqueID UPPER_KICK_LAND = 405316760U;
     } // namespace EVENTS
 
     namespace GAME_PARAMETERS
