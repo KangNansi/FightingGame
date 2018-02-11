058d82b915e46e2fbf4115c45883e29d89a0c73e
Boby <alexandre.bobe@laposte.net>
2018-02-11 17:24:21 +0100

    La soundbank des sons dans la banque

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,15 +13,26 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID BACK_KICK_INIT = 4195553302U;
+        static const AkUniqueID BACK_KICK_WHOOSH = 3992044314U;
         static const AkUniqueID BLOCK = 4001580976U;
+        static const AkUniqueID FORWARD_INIT = 1593604493U;
+        static const AkUniqueID FORWARD_WHOOSH = 17683061U;
+        static const AkUniqueID JAB = 780552104U;
+        static const AkUniqueID KILL = 2030840469U;
+        static const AkUniqueID LOW_KICK = 622371194U;
         static const AkUniqueID MID_HIT = 1711038907U;
         static const AkUniqueID PARRY_11 = 4268837494U;
         static const AkUniqueID PERFECT_PARRY_10 = 3368579805U;
+        static const AkUniqueID ROUND_START = 280570306U;
+        static const AkUniqueID UPPER_KICK = 321053796U;
+        static const AkUniqueID UPPER_KICK_HANDPAT = 587916051U;
     } // namespace EVENTS
 
     namespace GAME_PARAMETERS
     {
         static const AkUniqueID GUARDBREAK = 3067181449U;
+        static const AkUniqueID KILL = 2030840469U;
     } // namespace GAME_PARAMETERS
 
     namespace BANKS
@@ -35,6 +46,11 @@ namespace AK
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
         static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
     } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID DELAY = 357718954U;
+    } // namespace AUX_BUSSES
 
 }// namespace AK
 
